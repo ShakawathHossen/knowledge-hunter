ae798b4f744e56927b6ba9ac7fc59baaa70a8308
ShakawathHossen <shakawath15-2483@diu.edu.bd>
2023-03-31 05:53:06 +0600

    bookmarked added updated

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import './Blog.css'
 
 const Blog = (props) => {
-    const { id, name, blogTitle, readTimestamp, publishedDate, authorImage, blogImage } = props.blog;
-    console.log(props.blog);
+    const { id, name, blogTitle, readTimestamp, publishedDate, authorImage, blogImage,postDate } = props.blog;
+    const updateBookmark=props.updateBookmark;
+    // console.log(props);
+  
     return (
         <div>
             <div className="card bg-base-100 w-11/12 shadow-lg shadow-indigo-500/40  border-2 p-3 my-6">
@@ -18,8 +21,8 @@ const Blog = (props) => {
                         </div>
                     </div>
                     <div className='flex gap-3'>
-                        <p className='text-slate-400'>0{readTimestamp} min read</p>
-                        <span className='bookmark'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <p className='text-slate-400'>{readTimestamp} min read</p>
+                        <span id='bookmark-icon' onClick={()=>updateBookmark(props.blog)} className='bookmark'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
                         </svg>
                         </span>
--- src/Components/BlogContainer/BlogContainer.jsx
+++ src/Components/BlogContainer/BlogContainer.jsx
@@ -3,21 +3,44 @@ import Blog from '../Blog/Blog';
 
 const BlogContainer = () => {
     const [blogs,setBlogs]=useState([])
+    const [bookmark,setBookmark]=useState([])
+
     useEffect(()=>{
         fetch('data.json')
         .then (res=>res.json())
         .then(data=>setBlogs(data))
 
     },[])
+    // bookmark handle
+    const updateBookmark = (blog)=> {
+        const newBookmark =[...bookmark,blog];
+        setBookmark(newBookmark);
+
+     } 
+    // bookmark handle 
+
     return (
-        <div className='md:w-8/12 mx-auto flex'>
-          <div className='w-8/12'>
+        <div className='md:w-10/12 mx-auto flex justify-between'>
+          <div className='w-80%'>
           {
-                blogs.map(blog=><Blog blog={blog} key={blog.id}></Blog>)
+                blogs.map(blog=><Blog blog={blog} key={blog.id} updateBookmark={updateBookmark}></Blog>)
             }
           </div>
           <div>
-            <h1>BookMark</h1>
+            <div  className='w-20% my-6'>
+            <div className="border-2 px-7 py-5 rounded mb-4">
+                <p className='text-md font-bold text-blue-500'>Spent time on read: 177 min</p>
+
+            </div>
+            <div className="border-2 px-5 py-3 rounded  bg-slate-200">
+                <p className='text-md pb-5 font-bold'>Bookmarked Blogs : {bookmark.length}</p>
+                <p className='text-md font-semibold bg-white px-7 py-5'>{bookmark.name}</p>
+
+
+            </div>
+
+            </div>
+    
           </div>
         </div>
     );
